136ad99d766b50e242891e9431d2e0e34ab28899
Alexandra Puchianu <alexandrapuchianu@Alexandras-MacBook-Pro.local>
2023-05-30 11:48:17 +0300

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("postman-request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoiYWxleGFuZHJhMjE5OSIsImEiOiJjbGh5d2Z5ZjYxODJ4M21wZ3Q5d3h6OXY1In0.3uU7J2J-VcI8JN4RAdOfmQ&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        longitude: body.features[0].center[0],
+        latitude: body.features[0].center[1],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
